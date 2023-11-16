998c37b0f5052424aae249edfdf2e545ae578960
SweetId <2630750+SweetId@users.noreply.github.com>
2023-11-16 19:07:36 +0530

    updating revision

--- packages/n/nazaraimgui/xmake.lua
+++ packages/n/nazaraimgui/xmake.lua
@@ -7,7 +7,7 @@ package("nazaraimgui")
 
 	add_urls("https://github.com/SweetId/NazaraImgui.git")
 
-	add_versions("2023.11.16-3", "31f8b03a229f132792726e0a605fafa5b8ec281e")
+	add_versions("2023.11.16-4", "00fcb37e4aaa88772a4c65977307394f46dd6827")
 	
 	add_deps("nazarautils", "nazaraengine")
 	add_deps("imgui v1.87-docking")
